98c09b021a95cabe9c35cca0d9979ec5dd9c4259
Samuel Alvarez <samuelralvarez2004@gmail.com>
2026-02-10 13:45:12 -0600

    reformatted layout

--- portfolio/src/app/components/navbar/Navbar.jsx
+++ portfolio/src/app/components/navbar/Navbar.jsx
@@ -2,10 +2,10 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <nav className="bg-[#FF0000] font-[Caslon] text-2xl font-bold w-full h-15 fixed top-0 left-0 flex flex-row justify-between items-center px-30">
+    <nav className="bg-[#AAC4F5] text-[white] font-[Caslon] text-2xl font-bold w-full h-15 fixed top-0 left-0 flex flex-row justify-between items-center px-20">
       <div>
         <a href="#name" className="">
-          Samuel Alvarez
+          Sam Alvarez
         </a>
       </div>
 
@@ -21,7 +21,7 @@ const Navbar = () => {
             <a href="#projects">Projects</a>
           </li>
           <li>
-            <a href="/Resume_Samuel_Alvarez_2025.pdf">Resume</a>
+            <a href="/Resume_Samuel_Alvarez_2025.pdf" target="_blank">Resume</a>
           </li>
         </ul>
       </div>
--- portfolio/src/app/page.js
+++ portfolio/src/app/page.js
@@ -6,48 +6,39 @@ export default function Home() {
     <>
       <Navbar />
 
-      <div className="bg-[#16C2F9] w-600 h-600 rounded-full -ml-25 -mt-400">
-        <div className="bg-[#007FA8] w-590 h-590 rounded-full ml-5 mt-5">
-          <div className="bg-[#16C2F9] w-550 h-550 rounded-full ml-25 mt-25">
-
-          </div>
-        </div>
-      </div>
-
-      <section id="name" className="mb-125">
-        <div className="text-8xl font-bold text-white italic ml-25 -mt-190">
-          Samuel Alvarez
+      <section id="name" className="mb-125 scroll-mt-[285]">
+        <div className=" text-[#AAC4F5] text-8xl font-bold italic ml-25 mt-70">
+          Sam Alvarez
         </div>
 
-        <div className="text-xl text-white font-bold ml-25">
+        <div className=" text-[#AAC4F5] text-xl font-bold ml-25">
           Hello, I'm a full-stack developer based in Houston, TX 
         </div>
       </section>
 
-      <section id="aboutme" className="">
-        <div className="text-5xl font-bold text-white ml-25 mt-25 mb-5">
+      <section id="aboutme" className="mb-125 scroll-mt-[180]">
+        <div className="text-[#AAC4F5] text-5xl font-bold italic ml-25 mt-25 mb-5">
           About Me
         </div>
 
-        <div className="flex flex-row">
+        <div className="flex flex-row justify-center items-center">
           <div>
-          <img src="/Sam_photo.webp" className="w-50"></img>
+          <img src="/Sam_photo.webp" className="w-50 rounded-lg"></img>
           </div>
 
-          <div className="text-xl text-white font-bold w-175 ml-25">
-            I'm a passionate full-stack developer who excels in group settings. I also love experimenting with different languages, such as Java,
-            Ruby, Lua, Erlang, Elixir, Kotlin, and Python. When I'm not programming I enjoy drawing, playing Chess, or playing video games.
+          <div className="bg-[#AAC4F5] text-white text-xl font-bold w-175 ml-25 p-[10] rounded-lg ">
+            I am a Senior at the University of Houston pursuing a degree in Computer Science with a minor in Data and Society. I am passionate about web developement, however I have experience with tutoring and data science as well.
           </div>
         </div>
       </section>
 
 
 
-      <div className="text-5xl font-bold text-white ml-25 mt-25">
+      <div className="text-[#AAC4F5] text-5xl font-bold ml-25 mt-25">
         Skills
       </div>
 
-      <div className="text-5xl font-bold text-white ml-25 mt-25">
+      <div className="text-[#AAC4F5] text-5xl font-bold ml-25 mt-25">
         Projects
       </div>
     </>
